b91f6d40930f3cab6425b15cc6049b6643ff8c64
smashwilson <smashwilson@gmail.com>
2010-11-07 15:31:20 -0500

    Read word counts and quotas from a data file.

--- words.rb
+++ words.rb
@@ -1,6 +1,12 @@
-@starting = 9119
-@quota = 2262
-@target = @starting + @quota
+@goal_string = File.open('wordcount.dat') { |f| f.read nil }
+
+@starting = @goal_string[/starting\W+(\d+)/, 1].to_i
+if @goal_string =~ /target\W+(\d+)/
+  @target = $1.to_i
+else
+  quota_str = @goal_string[/quota\W+(\d+)/, 1] || '1667'
+  @target = @starting + quota_str.to_i
+end
 
 @current = `wc -w chapter*.txt | grep total`.to_i
 puts "Current count: #{@current}"
