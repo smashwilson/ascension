c4f3aee4de0e2b2328bf1ba0a5901aceb03527d3
smashwilson <smashwilson@gmail.com>
2010-11-07 11:54:40 -0500

    Updating the word count, double-checking my git setup.

--- words.rb
+++ words.rb
@@ -1,7 +1,8 @@
-@starting = 6790
+@starting = 9119
 @quota = 2262
 @target = @starting + @quota
 
 @current = `wc -w chapter*.txt | grep total`.to_i
 puts "Current count: #{@current}"
+puts "Today's target: #{@target}"
 puts "Remaining today: #{@target - @current}"
