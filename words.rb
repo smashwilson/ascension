38b16c0a621c52ac99acd57c818a1d91560e84d1
smashwilson <smashwilson@gmail.com>
2010-11-14 13:06:22 -0500

    Now the words script auto-updates.

--- words.rb
+++ words.rb
@@ -1,21 +1,26 @@
-@goal_string = File.open('wordcount.dat') { |f| f.read nil }
+loop do
+  @goal_string = File.open('wordcount.dat') { |f| f.read nil }
 
-@starting = @goal_string[/starting\W+(\d+)/, 1].to_i
-if @goal_string =~ /target\W+(\d+)/
-  @target = $1.to_i
-  @quota = @target - @starting
-else
-  @quota = (@goal_string[/quota\W+(\d+)/, 1] || 1667).to_i
-  @target = @starting + @quota
+  @starting = @goal_string[/starting\W+(\d+)/, 1].to_i
+  if @goal_string =~ /target\W+(\d+)/
+    @target = $1.to_i
+    @quota = @target - @starting
+  else
+    @quota = (@goal_string[/quota\W+(\d+)/, 1] || 1667).to_i
+    @target = @starting + @quota
+  end
+
+  @current = `wc -w chapter*.txt | grep total`.to_i
+  @progress = @current - @starting
+
+  @percent = @progress * 100.0 / @quota
+
+  system 'cls'
+  puts "Starting point: #{@starting}"
+  puts "Today's target: #{@target}"
+  puts "Remaining today: #{@target - @current}"
+  puts
+  puts "Current count: #{@current} [#{@progress}/#{@quota}] [%.3f%%]" % @percent
+
+  sleep 1
 end
-
-@current = `wc -w chapter*.txt | grep total`.to_i
-@progress = @current - @starting
-
-@percent = @progress * 100.0 / @quota
-
-puts "Starting point: #{@starting}"
-puts "Today's target: #{@target}"
-puts "Remaining today: #{@target - @current}"
-puts
-puts "Current count: #{@current} [#{@progress}/#{@quota}] [#{@percent}%]"
