39bbe4f13e8ba29d0a38321d9030ae2f3d3b5dec
smashwilson <smashwilson@gmail.com>
2010-11-18 20:51:09 -0500

    The word counter can run in autoupdate or single-shot mode.

--- words.rb
+++ words.rb
@@ -1,4 +1,18 @@
-loop do
+#!/bin/env ruby
+# Word count script for Nanowrimo '10
+
+require 'optparse'
+
+@options = { :update => false }
+OptionParser.new do |opts|
+  opts.banner = "Usage: #{$0} [options]"
+
+  opts.on('-u', '--update', 'Continuously update the screen.') do
+    @options[:update] = true
+  end
+end.parse!
+
+def count
   @goal_string = File.open('wordcount.dat') { |f| f.read nil }
 
   @starting = @goal_string[/starting\W+(\d+)/, 1].to_i
@@ -16,11 +30,17 @@ loop do
   @percent = @progress * 100.0 / @quota
 
   system 'cls'
+  puts "  Current Word Count"
+  puts
   puts "Starting point: #{@starting}"
   puts "Today's target: #{@target}"
   puts "Remaining today: #{@target - @current}"
   puts
   puts "Current count: #{@current} [#{@progress}/#{@quota}] [%.3f%%]" % @percent
+end
 
-  sleep 1
+if @options[:update]
+  loop { count ; sleep 1 }
+else
+  count
 end
